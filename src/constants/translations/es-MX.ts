9f6481c3630ab328c5e9d5338b74ccebade91056
MR-Samy-726 <65687644+MR-Samy-726@users.noreply.github.com>
2022-07-25 19:42:44 -0600

    Spanish Translation
    There is a spanish translation.

---
+++ src/constants/translations/es-MX.ts
@@ -0,0 +1,68 @@
+import { Translation } from 'types/Translation';
+
+const es_MX: Translation = {
+  // DASHBOARD
+  CREDIT: 'Crédito',
+  DEBIT: 'Débito',
+  RECENT_TRANSACTIONS: 'Transacciones Recientes',
+  MY_ACCOUNTS: 'Mis Cuentas',
+
+  // ADD ACCOUNT
+  ADD_ACCOUNT: 'Añadir Cuenta',
+  ACCOUNT_NAME: 'Nombre de la Cuenta',
+  INITIAL_AMOUNT: 'Cantidad Inicial',
+  CREATE_ACCOUNT: 'Crear Cuenta',
+
+  // EDIT ACCOUNT
+  EDIT_ACCOUNT: 'Editar Cuenta',
+  UPDATE_ACCOUNT: 'Actualizar Cuenta',
+
+  // ACCOUNT DETAILS
+  ACCOUNT_DETAILS: 'Detalles de la Cuenta',
+  INITIAL_BALANCE: 'Balance Inicial',
+  DELETE_ACCOUNT: '¿Eliminar Cuenta?',
+  DELETE_ACCOUNT_INFO:
+    'Esto va a eliminar permanentemente la cartera virtual {{accountName}} incluyendo las {{transactionCount}} transacciones vinculadas.',
+
+  // NEW TRANSACTION
+  NEW_TRANSACTION: 'Nueva Transacción',
+  CREATE_TRANSACTION: 'Crear Transacción',
+  CATEGORY: 'Categoría',
+  SOURCE_ACCOUNT: 'Cuenta de Proveniencia',
+  DESTINATION_ACCOUNT: 'Cuenta de Destino',
+  TRANSFER: 'Transferencia',
+  SHORT_DESCRIPTION: 'Descripción Breve',
+  AMOUNT: 'Cantidad',
+
+  // EDIT TRANSACTION
+  EDIT_TRANSACTION: 'Editar la Transacción',
+  UPDATE_TRANSACTION: 'Actualizar la Transacción',
+
+  // TRANSACTION DETAILS
+  TRANSACTION_DETAILS: 'Detalles de la Transacción',
+  ACCOUNT: 'Cuenta',
+  DATE_CREATED: 'Fecha de Creación',
+  DATE_UPDATED: 'Fecha Actualizada',
+  DELETE_TRANSACTION: '¿Eliminar Transacción?',
+  DELETE_TRANSACTION_INFO:
+    'Esto va a eliminar permanentemente la transacción del historial.',
+  KEEP: 'Mantener',
+  DELETE: 'Eliminar',
+
+  // Transactions
+  TRANSACTIONS: 'Transacciones',
+  SEARCH: 'Buscar',
+  DATE_RANGE: 'Rango de Tiempo',
+  SHOW_ALL: 'Mostrar Todo',
+
+  // SETTINGS
+  SETTINGS: 'Ajustes',
+  TRANSLATION_NAME: 'Español (MX)',
+  CURRENCY: 'Divisa',
+  LANGUAGE: 'Idioma',
+  THEME: 'Tema',
+  THEME_LIGHT: 'Tema Claro',
+  THEME_DARK: 'Tema Oscuro',
+};
+
+export default es_MX;
